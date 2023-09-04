0cf0e9df81b5fc5407d9f513456fe9dd316b144f
lahirurajapaksha <ltwrajapaksha@gmail.com>
2023-09-04 12:48:43 +0530

    update gitignore file

--- model.py
+++ model.py
@@ -1,6 +1,5 @@
-#caridac-arrest prediction model
-
 import pandas as pd
+from dataGroupingAndPadding import dataGroupingAndPadding
 import numpy as np
 import random
 import keras.backend as K
@@ -16,52 +15,52 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn import metrics
 import argparse
 from sklearn.metrics import roc_curve, roc_auc_score, f1_score, precision_recall_curve
-from datPreProcessingAndGrouping import dataGroupingAndPreProcessing
-from modelPlots import plot_decision_tree, plot_lstm
 
-
-# Command line arguments
-parser =argparse.ArgumentParser(description='Early Warning Score for cardiac patients')
-parser.add_argument('--epocs', type=int, help='number of iteration', default=5)
-parser.add_argument('--lrate', type=float, help='learning rate', default=0.001)
-parser.add_argument('--model-name', type=str, help='name of the model')
-parser.add_argument('--batch-size', type=int, default=8)
-parser.add_argument('--lstm-nodes', type=int, default=2)
-
-args = parser.parse_args()
 epocs = 100
-learningRate = 0.001
-# modelName =args.model_name
+learningRate = 0.01
 batchSize = 10
 lstmNodes = 2
 
+ ## Dataset can be found in https://zenodo.org/record/7603772
+ ## Experiment data set. Import each experiment data set separately
+patients_training_data = pd.read_csv('/content/sample_data/ExperimentNo1Data.csv')
+patients_test_data = pd.read_csv('/content/sample_data/Test10%.csv') 
+patients_validation_data = pd.read_csv('/content/sample_data/Validate10%.csv') 
 
-NO_OF_FEATURES = 20
+time_step = int(patients_training_data.groupby('ID').count().mean()[0])
+no_of_patients = patients_training_data['ID'].max()
+no_of_test_patients = patients_test_data['ID'].max()
+no_of_validation_patients = patients_validation_data['ID'].max()
 
-patients_data = pd.read_csv('./cardiacPatientData.csv') ## Dataset can be found in https://zenodo.org/record/7603772
-time_step = int(patients_data.groupby('ID').count().mean()[0])
-no_of_patients = patients_data['ID'].max()
-newPatientList, decisionTreeData = dataGroupingAndPreProcessing(patients_data,time_step,NO_OF_FEATURES,no_of_patients)
+training_LSTM_data, training_decisionTree_data = dataGroupingAndPadding(patients_training_data,time_step,20,no_of_patients)
+testing_LSTM_data, testing_decisionTree_data = dataGroupingAndPadding(patients_test_data, time_step, 20,no_of_test_patients)
+validation_LSTM_data, validation_decisionTree_data = dataGroupingAndPadding(patients_validation_data,time_step,20,no_of_validation_patients)
 
 # Shuffle the data set in the same order
-newPatientList = newPatientList.tolist()
-decisionTreeData = decisionTreeData.tolist()
-shuffleDataSet = list(zip(newPatientList, decisionTreeData))
+training_LSTM_data = training_LSTM_data.tolist()
+training_decisionTree_data = training_decisionTree_data.tolist()
+shuffleDataSet = list(zip(training_LSTM_data, training_decisionTree_data))
 random.shuffle(shuffleDataSet)
-newPatientList, decisionTreeData = zip(*shuffleDataSet)
-newPatientList = np.asarray(newPatientList)
-decisionTreeData = np.asarray(decisionTreeData)
+training_LSTM_data, training_decisionTree_data = zip(*shuffleDataSet)
+training_LSTM_data = np.asarray(training_LSTM_data)
+training_decisionTree_data = np.asarray(training_decisionTree_data)
 
 # Prepare data set for LSTM model
-lstm_patient_training_data = newPatientList[:, :, 1:7]
-lstm_patient_training_data_label = newPatientList[:, :, -1]
+lstm_patient_training_data = training_LSTM_data[:, :, 1:7]
+lstm_patient_training_data_label = training_LSTM_data[:, :, -1].reshape(no_of_patients, time_step, 1)
 
-# Reshaping the label data
-lstm_patient_training_data_label = lstm_patient_training_data_label.reshape(no_of_patients, time_step, 1)
+# Test data set for LSTM model
+lstm_patient_test_data = testing_LSTM_data[:, :, 1:7]
+lstm_patient_test_data_label =testing_LSTM_data[:, :, -1].reshape(no_of_test_patients, time_step, 1)
+
+# Validation data set for LSTM model
+lstm_validation_data =validation_LSTM_data[:, :, 1:7]
+lstm_validation_data_label =validation_LSTM_data[:, :, -1].reshape(no_of_validation_patients,time_step,1)
+
 
 # Prepare data set for Decision Tree
-d_tree_training_data = decisionTreeData[:, 7:19]
-d_tree_training_data_label = np.reshape(decisionTreeData[:, -1], (no_of_patients, 1))
+d_tree_training_data = training_decisionTree_data[:, 7:19]
+d_tree_training_data_label = np.reshape(training_decisionTree_data[:, -1], (no_of_patients, 1))
 
 # Defining the model
 model = Sequential()
@@ -69,28 +68,35 @@ model.add(LSTM(lstmNodes, return_sequences=True, input_shape=(time_step, 6)))
 model.add(Dense(1, activation='linear'))
 
 # Compile the model
-model.compile(Adam(lr=learningRate), loss='binary_crossentropy', metrics=['acc'])  # lr0.005 0r 0.001
+model.compile(Adam(learning_rate=learningRate), loss='binary_crossentropy', metrics=['acc'])  # lr0.005 0r 0.001
 model.summary()
 
 # Fit the model LSTM
 history = model.fit(lstm_patient_training_data, lstm_patient_training_data_label,
-                    shuffle=False, validation_split=0.3, batch_size=batchSize, epochs=epocs)
+                    shuffle=False, validation_data=(lstm_validation_data,lstm_validation_data_label), batch_size=batchSize, epochs=epocs)
+
 acc = np.round(history.history['acc'][epocs-1], 2)
 val_acc = np.round(history.history['val_acc'][epocs-1], 2)
-
-print('**********Summary*************')
+print('###Model Summary###')
 print(f'Accuracy {np.round(acc, 3)} ValidationAccuracy {np.round(val_acc, 3)}')
 modelName = f'acc:{acc} val_acc:{val_acc} epocs:{epocs} nodes:{lstmNodes}'
-model.save('./ModelSaves/FinalTrainedModels/'+modelName+'.h5')
+
 
 # Getting the latent vector space from lstm layers
 outputs = []
 for layer in model.layers:
     keras_function = K.function([model.input], [layer.output])
-    outputs.append((keras_function([lstm_patient_training_data, 1])))
+    outputs.append((keras_function([lstm_patient_training_data])))
 latentVectorSpace = (outputs[0][0])[:, (time_step-1), -1]
 latentVectorSpace = np.reshape(latentVectorSpace, (no_of_patients, 1))
-d_tree_training_data_combined = np.append(d_tree_training_data, latentVectorSpace, axis=1)#Append latent vector with other data
+
+#Append latent vector with other data
+d_tree_training_data_combined = np.append(d_tree_training_data, latentVectorSpace, axis=1)
+
+test_loss, test_accuracy = model.evaluate(lstm_patient_test_data, lstm_patient_test_data_label)
+
+print(f"Test Loss: {test_loss}")
+print(f"Test Accuracy: {test_accuracy}")
 
 # Feature selection for decision tree model
 feature_cols = ['Age', 'Gender', 'GCS', 'Na', 'K', 'Cl', 'Urea', 'Creatinine', 'Alcoholic', 'Smoke',
@@ -129,6 +135,3 @@ d_tree_false_positive, d_tree_true_positive, d_tree_treshold = roc_curve(y_test,
 d_tree_precision, d_tree_recall, d_tree_recall_threshold = precision_recall_curve(y_test, y_pred)
 d_tree_auc_score = roc_auc_score(y_test, y_pred)
 d_tree_f1_score = f1_score(y_test,y_pred)
-
-
-
