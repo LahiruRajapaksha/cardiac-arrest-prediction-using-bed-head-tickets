43961ba0f705c821747e54f360aacea2a163affb
lahirurajapaksha <ltwrajapaksha@gmail.com>
2023-09-04 12:44:55 +0530

    update the model

--- SyntheticDataVault.py
+++ SyntheticDataVault.py
@@ -2,7 +2,8 @@ import pandas as pd
 from sdv.metadata import SingleTableMetadata
 from sdv.sequential import PARSynthesizer
 
-patient_record = pd.read_csv('/content/sample_data/Train_Survived.csv')
+patient_record = pd.read_csv('/content/sample_data/TrainDead.csv')
+# patient_record = pd.read_csv('/content/sample_data/TrainSurvived.csv')
 
 metadata = SingleTableMetadata()
 metadata.detect_from_dataframe(data=patient_record)
@@ -10,24 +11,16 @@ metadata.update_column(
     column_name='ID',
     sdtype='id')
 metadata.set_sequence_key(column_name='ID')
-# print(metadata)#
-# sequence = patient_record[patient_record['ID'] == 1]
-# sequence
-
-#
-# patient_record['ID'].unique()
-
 
 synthesizer = PARSynthesizer(
     metadata,
-    context_columns=['Age',	'Gender', 'Alcoholic','FHCD','Smoke','TriageScore','Outcome'],
-    epochs=2,
+    context_columns=['Age','Gender','Alcoholic','Smoke','FHCD','TriageScore','Outcome'],
+    epochs=90,
     verbose=True,
     )
-
 
 synthesizer.fit(patient_record)
 
 #
-synthetic_data = synthesizer.sample(num_sequences=2)
-print(synthetic_data)
+synthetic_data = synthesizer.sample(num_sequences=74)
+synthetic_data.to_csv("/content/sample_data/FS74.csv")
